09521ac325aab59837161351cb63e845356bc3ab
Hopson97 <mhopson@hotmail.co.uk>
2020-06-19 22:16:07 +0100

    Add the rest of the instructions i guess

--- main.go
+++ main.go
@@ -16,27 +16,37 @@ type Gorbitsa struct {
 
 type Instruction struct {
 	op    byte
-	param int
+	param uint8
 }
 
 func (gorbitsa *Gorbitsa) hasNext() bool {
-	return gorbitsa.regPc < 256
+	return gorbitsa.regPc < 255
 }
 
 func (gorbitsa *Gorbitsa) executeNext(instructions [256]Instruction) {
-	switch instructions[gorbitsa.regPc].op {
+	var instruction = &instructions[gorbitsa.regPc]
+	switch instruction.op {
 	case 'G':
+		gorbitsa.regX = gorbitsa.memory[instruction.param]
 	case 'O':
+		gorbitsa.memory[instruction.param] = gorbitsa.regX
 	case 'R':
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Input: ")
 		gorbitsa.regX, _ = reader.ReadByte()
 	case 'B':
+		if gorbitsa.regX == 0 {
+			gorbitsa.regPc = uint32(gorbitsa.memory[instruction.param])
+		}
 	case 'I':
+		gorbitsa.regX += instruction.param
 	case 'T':
-		fmt.Println(gorbitsa.regX - '0')
+		fmt.Print(gorbitsa.regX)
 	case 'S':
+		gorbitsa.regX = instruction.param
 	case 'A':
+		gorbitsa.regX += gorbitsa.memory[instruction.param]
+
 	}
 	gorbitsa.regPc++
 }
@@ -45,8 +55,8 @@ func newInstruction(instr string) Instruction {
 	var instruction = Instruction{}
 	instruction.op = instr[0]
 	i, err := strconv.Atoi(instr[1:len(instr)])
-	if err != nil {
-		instruction.param = i
+	if err == nil {
+		instruction.param = uint8(i)
 	}
 	return instruction
 }
@@ -60,7 +70,7 @@ func compileProgram(program string) [256]Instruction {
 }
 
 func main() {
-	const program = "R T"
+	const program = "S72 T S101 T S108 T T S111 T S32 T S87 T S111 T S114 T S108 T S100 T"
 	var instructions = compileProgram(program)
 
 	var gorbitsa = Gorbitsa{}
